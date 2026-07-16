9ee89c29a0686cded814bff6ae4a6fc9d741cd58
iiacchoi-ai <iiacchoi@gmail.com>
2026-07-17 02:02:13 +0900

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,7 @@
+const C="pushback-v1";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./manifest.json","./icon-192.png"]))); self.skipWaiting();});
+self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))); self.clients.claim();});
+self.addEventListener("fetch",e=>{
+  e.respondWith(fetch(e.request).then(r=>{const cl=r.clone(); caches.open(C).then(c=>c.put(e.request,cl)); return r;})
+  .catch(()=>caches.match(e.request,{ignoreSearch:true}).then(m=>m||caches.match("./"))));
+});
