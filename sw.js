e841f0b6593a111c97416bc723e9d258bb6e4093
iiacchoi-ai <iiacchoi@gmail.com>
2026-08-01 23:10:27 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const C="pushback-v7";
-self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./manifest.json","./icon-192.png"]))); self.skipWaiting();});
+const C="pushback-v82";
+self.addEventListener("install",e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(["./","./gates.js","./manifest.json","./icon-192.png","./img/ac/ac_red.png","./img/ac/ac_green.png","./img/ac/ac_magenta.png","./img/ac/ac_sky.png","./img/ac/ac_white.png"]))); self.skipWaiting();});
 self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!==C).map(x=>caches.delete(x))))); self.clients.claim();});
 self.addEventListener("fetch",e=>{
   e.respondWith(fetch(e.request).then(r=>{const cl=r.clone(); caches.open(C).then(c=>c.put(e.request,cl)); return r;})
